8f69b38df1051917f7ed571761b8ba2d1586c765
nhn <nghianh.b22dt223@stu.ptit.edu.vn>
2025-12-06 03:13:12 +0100

    fix: only env fe url is allowed to connect be

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 app.use(express.json());
